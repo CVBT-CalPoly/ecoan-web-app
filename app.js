d2ef2d3c01c699cbe415e3ad01dfb4653a23baf2
Alex Ye <alexzye1@gmail.com>
2017-02-19 21:22:35 -0800

    Add temp favicon

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-// var Sequelize = require('sequelize')
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 var table = require('./routes/table');
-var db = require('./models/db');
+
 
 
 // Create the application
@@ -17,8 +17,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
