398d6af179ea56e6249c89d1a521ca2fbad78aee
Alex Ye <alexzye1@gmail.com>
2017-03-13 17:29:42 -0700

    Add table detailing (border, CRUD not sortable, etc)

--- public/js/table.js
+++ public/js/table.js
@@ -1,6 +1,12 @@
 $(document).ready( function () {
    $('#data-table').DataTable({
-      "scrollX": true
+      "scrollX": true,
+      "columnDefs": [ {
+            "searchable": false,
+            "orderable": false,
+            "targets": 0
+        } ],
+        "order": [[ 1, 'asc' ]]
    });
    $('#data-table-container').fadeIn("fast");
 } );
