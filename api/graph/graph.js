b328563e5f7c2342fbd5290293157ee2fa40593f
Nathan Sylvia <nsylvia339@gmail.com>
2017-03-23 10:41:13 -0700

    product cost breakdown almost done

--- api/graph/graph.js
+++ api/graph/graph.js
@@ -50,4 +50,23 @@ router.get('/investmentPayback', function(req, res) {
     res.send(JSON.stringify(results));
   });
 });
+
+router.get('/prodName', function(req, res) {
+  var id = req.url.substring(req.url.search(":")+4, req.url.search("}")-3);
+  db.sequelize.query("select ProdEngName from Products where ProdNo = " + id)
+  .spread(function(results, metadata) {
+    res.send(JSON.stringify(results));
+  });
+});
+
+router.get('/pieceCosts', function(req, res) {
+  console.log(req.url);
+  var id = req.url.substring(req.url.search(":")+4, req.url.search("}")-3);
+  db.sequelize.query("select EngName, CostSuperType, PieceCost from ProdCosts where ProdNo = " + id + " and CostYear is not NULL")
+  .spread(function(results, metadata) {
+    res.send(JSON.stringify(results));
+  });
+});
+
+
 module.exports = router;
