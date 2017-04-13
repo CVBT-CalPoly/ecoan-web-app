454be47fe72d1c44cfe9d32fb2f96e151aa6df8d
Alex Ye <alexzye1@gmail.com>
2017-04-12 17:23:40 -0700

    Add delete row from table API

--- api/tables/crud.js
+++ api/tables/crud.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../../models/db');
+var helper = require('../../routes/tablehelper');
 
 /* GET home page. */
 router.post('/delete/:table', function(req, res) {
+  helper.getDbObject(req.params.table).destroy({
+    where: req.body
+  }).then(function(instance){
+    // console.log();
+    res.send("Deleted: " + parseInt(instance));
+  });
+});
+
+router.post('/edit/:table', function(req, res) {
   console.log(req.body)
   console.log(req.params.table);
   res.send("Success");
