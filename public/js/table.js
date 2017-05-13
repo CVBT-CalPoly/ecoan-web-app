5852d17e91335cb2457cb3c1c6665fee5c13eb8a
Jonathan Molina <nemolina@calpoly.edu>
2017-05-12 22:33:13 -0700

    initial capturing of filters

--- public/js/table.js
+++ public/js/table.js
@@ -41,12 +41,13 @@ $(document).ready( function () {
     // Apply the search for individual columns
     table.columns().every( function () {
         var that = this;
- 
+        // console.log(this.value);
         $( 'input', this.footer() ).on( 'keydown', function (ev) {
             if (ev.keyCode == 13) { // only search upon enter keypress (code 13)
             that
                 .search( this.value )
                 .draw();
+            console.log(this.value);
          }
         } );
     } );
@@ -110,11 +111,11 @@ $(document).ready( function () {
 
   $('#adv-filter-button').on('click', function() {
     $('#adv-filter-form').empty();
-
     var headers = $("table tr:eq(0) td");
+    var inputs = [];
 
     for (var ndx = 0; ndx < headers.length; ndx++) {
-      console.log(headers[ndx]);
+      // console.log(headers[ndx]);
       var header = headers[ndx];
       var row = document.createElement("div");
       var col1 = document.createElement("div");
@@ -126,6 +127,8 @@ $(document).ready( function () {
       var submit = document.createElement("input");
       var selectLabel = document.createElement("label");
       var select = document.createElement("select");
+      const rowName = header.innerHTML;
+      var inputObject = {};
 
       row.className = "row";
       row.appendChild(col1);
@@ -135,16 +138,19 @@ $(document).ready( function () {
       col1.appendChild(rowLabel);
       col1.appendChild(divInputGroup);
 
-      rowLabel.innerHTML = header.innerHTML;
+      rowLabel.innerHTML = rowName;
 
       divInputGroup.className = "input-group";
 
       input.className = "input-group-field"
+      input.id = rowName + "-input-value";
       input.type = "text";
+      inputObject["input"] = input.id;
 
       divButton.className = "input-group-button";
 
       submit.className = "button";
+      submit.id = rowName + "-fitler";
       submit.type = "submit";
       submit.value = "Filter";
 
@@ -157,19 +163,52 @@ $(document).ready( function () {
       col2.appendChild(select);
 
       selectLabel.innerHTML = "Filter Options";
+      select.id = rowName + "-select";
+      inputObject["select"] = select.id;
 
       var options = ["Contains", "Equals", "Starts with", "More than", "Less than", "Between", "Empty",
         "Doesn't contain", "Doesn't equal", "Doesn't start with", "Is not more than", "Is not between", "Is not empty"];
 
+      var isFirst = true;
       options.forEach(function(option) {
         var element = document.createElement("option");
+        if (isFirst) {
+          isFirst = false;
+          element.selected = "selected";
+        }
         element.value = option;
         element.innerHTML = option;
         select.appendChild(element);
       });
 
       $("#adv-filter-form").append(row);
+      inputs.push(inputObject);
     }
+
+    $('#adv-filter-form').on({
+      'valid invalid submit': function(g) {
+        g.stopPropagation();
+        g.preventDefault();
+
+        inputs.forEach(function(element) {
+          var input = $('#'+element.input).val();
+          var select = $('#'+element.select).val();
+          console.log("input: " + input);
+          console.log("select: " + select);
+
+        });
+
+        if (g.type === "submit") {
+          // AJAX call
+          console.log("make ajax call");
+        }
+      }
+    });
+
+    $('input[name="input-name"]').on('valid', function() {
+      // Handle the submission of the form
+      console.log('input-name');
+    });
   });
 
   $('#edit-button').on('click', function() {
