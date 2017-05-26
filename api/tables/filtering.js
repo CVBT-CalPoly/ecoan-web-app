b733b54231ce6cf51aa3639aaa4170ff3c619f48
Jonathan Molina <nemolina@calpoly.edu>
2017-05-26 14:50:27 -0700

    refactored table processing files

--- api/tables/filtering.js
+++ api/tables/filtering.js
@@ -1,9 +1,7 @@
-var express = require('express');
-var router = express.Router();
-var db = require('../../models/db');
-var helper = require('../../routes/tablehelper');
-var fs = require('fs');
-var drawCounter = 1;
+const express = require('express');
+const router = express.Router();
+const helper = require('../../routes/tablehelper');
+const processingHelper = require("../../api/tables/processinghelper");
 const filterSymbols = {
   "Contains": {"operator": "$like", "not": false, "prefix": "%", "suffix": "%"},
   "Equals": {"operator": "$eq", "not": false},
@@ -20,83 +18,16 @@ const filterSymbols = {
   "Is not empty": {"operator": "$eq", "not": true, "value": null},
 };
 
+// POST
 router.post('/:table', function(req, res) {
-  const dbObject = helper.getDbObject(req.params.table);
   const tableColumns = helper.getTableHeaders(req.params.table);
   var filter = getFilterOptions(req.body, tableColumns);
 
-  if (typeof(req.body['search[value]']) !== "undefined") {
-    console.log("search value found");
-    filter["$or"] = helper.getSearchObject(req.params.table, req.body['search[value]'], req.body);
-  }
-  else {
-    filter["$or"] = [];
-  }
-  console.log(req.body);
-  console.log("$or:\n" + filter["$or"]);
-
-  dbObject.findAndCount({
-    attributes: tableColumns,
-    limit: parseInt(req.body.length),
-    offset: parseInt(req.body.start),
-    // order: [
-    //   [helper.getColumnNameForTable(req.body.table, parseInt(req.body["order[0][column]"])), req.body["order[0][dir]"]] // e.g. ["ProdNo", "desc"]
-    // ],
-    raw: true,
-    // where: {
-    //   Rel_to_Base: { $between: [0, 3.9] } 
-    // }
-    where: filter
-  }).then(function(results) {
-    let response = {
-      "draw": drawCounter++,
-      "recordsTotal": results.count,
-      "recordsFiltered": results.count
-    };
-    let dataArray = [];
-    let rows = results.rows;
-    var rowNum = 0;
-
-    // format data response for DataTables into array of array objects [[], []]
-    rows.forEach(function(data) {
-      const keys = Object.keys(data);
-      let rowData = [];
-      var rowObject = {};
-      var ndx = 0;
-
-      keys.forEach(function(key) {
-        const value = data[key];
-
-        // check if image blob stored in database (which is retrieved as a buffer)
-        if (Buffer.isBuffer(value)) {
-          var fileName = "image" + rowNum++ + ".jpeg"; // increment counter so each row refers to a different image
-
-          fs.writeFile('public/images/'+fileName, value, 'utf8', function(err) {
-            if (err) {
-              console.log(err);
-            }
-          });
-          var seconds = new Date().getTime(); // appending seconds will advise browser not to use cached image
-          rowData.push("<img src=\"../../images/"+fileName+"?"+ seconds +"\" />"); // cell will display image
-        }
-        else {
-          rowData.push(value);
-          // rowObject[tableColumns[ndx++]] = value;
-        }
-      });
-      dataArray.push(rowData);
-      // dataArray.push(rowObject);
-    });
-    
-    response["data"] = dataArray;
-    res.send(JSON.stringify(response));
-  });
-
-  // res.send("Success");
+  processingHelper.processFilterTable(req, res, req.params.table, filter);
 });
 
 // Builds the filter object according to this http://docs.sequelizejs.com/manual/tutorial/querying.html
-// example:
+// Example:
 // {
 //   MixNo: {
 //     $gte: 45
@@ -111,11 +42,6 @@ function getFilterOptions(inputs, columns) {
     const value = inputs["inputs["+ndx+"][input]"];
     const value2 = inputs["inputs["+ndx+"][input2]"];
     const filterOption = filterSymbols[inputs["inputs["+ndx+"][select]"]];
-
-    console.log("ndx: " + ndx);
-    console.log("rowName: " + rowName);
-    console.log("value: " + value);
-    console.log("option: " + inputs["inputs["+ndx+"][select]"]);
 
     // check if there is input (excludes empty and blank strings)
     if (/\S/.test(value)) {
@@ -158,7 +84,6 @@ function getFilterOptions(inputs, columns) {
     }
   }
 
-  console.log("filter options:\n" + JSON.stringify(filter));
   return filter;
 }
 
--- api/tables/processing.js
+++ api/tables/processing.js
@@ -4,64 +4,11 @@ var db = require('../../models/db');
 var helper = require('../../routes/tablehelper');
 var fs = require('fs');
 var drawCounter = 1;
+const processingHelper = require("../../api/tables/processinghelper");
 
+// POST
 router.post('/', function(req, res) {
-  const tableColumns = helper.getTableHeaders(req.body.table);
-  const search = req.body['search[value]'];
-  const searchColumns = helper.getSearchObject(req.body.table, search, req.body);
-
-  helper.getDbObject(req.body.table).findAndCount({
-    attributes: tableColumns,
-    limit: parseInt(req.body.length),
-    offset: parseInt(req.body.start),
-    order: [
-      [helper.getColumnNameForTable(req.body.table, parseInt(req.body["order[0][column]"])), req.body["order[0][dir]"]] // e.g. ["ProdNo", "desc"]
-    ],
-    where: {
-      $or: searchColumns
-    },
-    raw: true,
-  }).then(function(results) {
-    let response = {
-      "draw": drawCounter++,
-      "recordsTotal": results.count,
-      "recordsFiltered": results.count
-    };
-    let dataArray = [];
-    let rows = results.rows;
-    var rowNum = 0;
-
-    // format data response for DataTables into array of array objects [[], []]
-    rows.forEach(function(data) {
-      const keys = Object.keys(data);
-      let rowData = [];
-      
-      keys.forEach(function(key) {
-        const value = data[key];
-
-        // check if image blob stored in database (which is retrieved as a buffer)
-        if (Buffer.isBuffer(value)) {
-          var fileName = "image" + rowNum++ + ".jpeg"; // increment counter so each row refers to a different image
-
-          fs.writeFile('public/images/'+fileName, value, 'utf8', function(err) {
-            if (err) {
-              console.log(err);
-            }
-          });
-          var seconds = new Date().getTime(); // appending seconds will advise browser not to use cached image
-          rowData.push("<img src=\"../../images/"+fileName+"?"+ seconds +"\" />"); // cell will display image
-        }
-        else {
-          rowData.push(data[key]);
-        }
-      });
-      dataArray.push(rowData);
-    });
-    
-    response["data"] = dataArray;
-    res.send(JSON.stringify(response));
-  });
-
+  processingHelper.processTable(req, res, req.body.table);
 });
 
 module.exports = router;
