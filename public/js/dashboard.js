bd47eba70475b46365e616877e691d1d93ae9d5c
Jonathan Molina <nemolina@calpoly.edu>
2017-06-01 13:48:41 -0700

    backups button only shows up if you are an admin

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,10 +1,18 @@
 $(document).ready( function () {
 
-  var backupButton = document.createElement('a');
+  $.ajax({
+    "url": "http://localhost:3000/api/user_data",
+    "type": "GET",
+    success: function(result) {
+      if (result.username.IsAdmin) {
+        var backupButton = document.createElement('a');
 
-  backupButton.className = "button expanded";
-  backupButton.href = "#";
-  backupButton.innerHTML = "View Backups";
+        backupButton.className = "button expanded";
+        backupButton.href = "#";
+        backupButton.innerHTML = "View Backups";
 
-  document.body.appendChild(backupButton);
+        document.body.appendChild(backupButton);
+      }
+    }
+  });
 });
