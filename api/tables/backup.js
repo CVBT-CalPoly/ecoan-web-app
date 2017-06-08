82723a99fe32bf99396097f902417ea3e50866e7
Jonathan Molina <nemolina@calpoly.edu>
2017-06-08 13:10:29 -0700

    retrieving db creditials from env

--- api/tables/backup.js
+++ api/tables/backup.js
@@ -9,10 +9,10 @@ router.post('/', function(req, res) {
     const filename = date.getMonth() + "-" + date.getDate() + "-" + date.getFullYear() + "-"+ date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "-backup-data.sql";
 
     mysqlDump({
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'ecoan',
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
         dest: "./backups/" + filename // destination file
     }, function(err) {
         // created data.sql file;
