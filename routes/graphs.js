6472ccab67bf461932a81dc054fc59b862cc6255
Jonathan Molina <nemolina@calpoly.edu>
2017-06-13 10:17:26 -0700

    fixed server error for charts

--- routes/graphs.js
+++ routes/graphs.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var db = require('../models/db');
 var helper = require('./tablehelper');
 var passport = require('passport');
+var localizer = require('./localizer');
 
 function isAuthenticated(req, res, next) {
   console.log("authing");
@@ -14,16 +15,43 @@ function isAuthenticated(req, res, next) {
 }
 router.get('/:graphs', isAuthenticated, function(req, res) {
   var tableName = req.params.graphs;
+
+  localizer.setLocale(req.user.locale);
   if( tableName == "breakeven") {
-    //  res.sendFile("../graphs/breakeven.html");
-    res.render('breakeven');
+    const breakeven = {
+      breakeven: {
+        chart: "BreakEven Chart"
+      }
+    };
+    localizer.translatePage(breakeven, {
+      next: function(values) {
+        res.render('breakeven', values);
+      }}
+    );
   }
   else if( tableName == "payback") {
-    res.render('payback');
+    const payback = {
+      payback: {
+        chart: "Payback Chart"
+      }
+    };
+    localizer.translatePage(payback, {
+      next: function(values) {
+        res.render('payback', values);
+      }}
+    );
   }
   else if( tableName == "productCostBreakdown") {
-    console.log("productCostBreakdown");
-    res.render('productcostbreakdown');
+    const breakdown = {
+      breakdown: {
+        chart: "Product Cost Breakdown Chart"
+      }
+    };
+    localizer.translatePage(breakdown, {
+      next: function(values) {
+        res.render('productcostbreakdown', values);
+      }}
+    );
   }
 });
 
